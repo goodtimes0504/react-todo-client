f1c2c84ba1804f428b63d49b4ebe2add2adce753
goodtimes0504 <revolution_lc@outlook.com>
2025-01-09 23:49:59 +0800

    feat:implement router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Routes,Route } from 'react-router'
+import Landing from './pages/landing/landing'
+import Register from './pages/Auth/Register'
+import Login from './pages/Auth/Login'
+import ToDoList from './pages/ToDo/ToDoList'
 
 export default function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Landing></Landing>} />
+      <Route path="/register" element={<Register></Register>} />
+      <Route path="/login" element={<Login></Login>} />
+      <Route path="/to-do-list" element={<ToDoList></ToDoList>} />
+    </Routes>
   )
 }
